36ee83a7685319125751b940346e9c32d3ddab2f
adala-wanyande <benardwanyande@gmail.com>
2024-02-01 04:19:27 +0300

    feat: show snackbar message upon expense item deletion

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -41,13 +41,40 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              _registeredExpenses.insert(expenseIndex, expense);
+            }),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
@@ -63,10 +90,8 @@ class _ExpensesState extends State<Expenses> {
           // Toolbar with the Add button
           const Text('The chart'),
           Expanded(
-              child: ExpensesList(
-            expenses: _registeredExpenses,
-            onRemoveExpense: _removeExpense,
-          )),
+            child: mainContent,
+          ),
         ],
       ),
     );
